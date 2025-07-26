7ad926fc3b7af26f98b1da9bc2231a34e6938bdb
Mary-Orlova <vampusha07@gmail.com>
2025-07-26 16:50:55 +0300

    капча для роботов, поиск атрибутов

--- 2/lesson7_step2.py
+++ 2/lesson7_step2.py
@@ -5,31 +5,29 @@ import time
 
 
 try:
-    link = "https://suninjuly.github.io/math.html"
+    link = "http://suninjuly.github.io/get_attribute.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
     def calc(x):
         return str(math.log(abs(12 * math.sin(int(x)))))
 
-    # x_element = browser.find_element(By.CSS_SELECTOR, "span#input_value.nowrap")
-    x_element = browser.find_element(By.ID, "input_value")
-    x = x_element.text
+    img_x = browser.find_element(By.ID, "treasure")
+    x_element = img_x.get_attribute("valuex")
+    print("x_element=", x_element)
+    x = x_element
     y = calc(x)
 
 
     # Заполняет поле значением Х по уникальному селектору
-    # answer= browser.find_element(By.CSS_SELECTOR, "input#answer.form-control")
-    answer = browser.find_element(By.ID, "answer")
+    answer= browser.find_element(By.ID, "answer")
     answer.send_keys(y)
 
     # Проставление галочки по уникальному селектору I'm the robot
-    # robot = browser.find_element(By.CSS_SELECTOR, "[for ='robotCheckbox']")
     robot = browser.find_element(By.ID, "robotCheckbox")
     robot.click()
 
     # Выбор в радио-баттен Robots rule
-    # robot_rule = browser.find_element(By.CSS_SELECTOR, "[for ='robotsRule']")
     robot_rule = browser.find_element(By.ID, "robotsRule")
     robot_rule.click()
 
