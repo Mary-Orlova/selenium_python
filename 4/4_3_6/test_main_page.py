aca0bc5c512f4cc48703abbcbb4085c55c7dc279
Mary-Orlova <vampusha07@gmail.com>
2025-12-07 19:15:27 +0300

    Группировка тестов setup

---
+++ 4/4_3_6/test_main_page.py
@@ -0,0 +1,18 @@
+import pytest
+
+from pages.main_page import MainPage
+from pages.basket_page import BasketPage
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
+        page.open()
+        page.should_be_login_link()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
+        page.open()
+        page.should_be_login_link()
